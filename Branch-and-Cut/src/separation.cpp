02d15111b28dff04aa1203d6a37beb654135ac66
Lucas Oliveira <lucas@q9.logq.int>
2025-08-26 15:41:34 -0300

    Falta o retorno da função

--- Branch-and-Cut/src/separation.cpp
+++ Branch-and-Cut/src/separation.cpp
@@ -5,11 +5,11 @@
 /***************************************************/
 #include "separation.h"
 #include <vector>
+#include <unordered_set>
 
 vector <vector<int> > MaxBack(double** x, int n){
     
-    vector<double> solution_init = {0}; //definir a solução inicial a partir da matriz
-    double temp;
+    vector<int> solution_init = {0}; //definir a solução inicial a partir da matriz
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             if(x[i][j] < EPSILON){
@@ -20,49 +20,70 @@ vector <vector<int> > MaxBack(double** x, int n){
         cout << "\n";
     }
 
-    cout << "Max_back: ";
+    //unordered_set <int> solSet(solution_init.begin(), solution_init.end());
+
+    vector<bool> connected(n, 0);
+    connected[0] = true;
+
+    cout << "\nMax_back: ";
 
     vector <double> maxback_val(n, 0); //variável que diz as conexões internas (?) -> fazer ele já como o número maior pode ajudar
-    //for pra definir maxback val
-
+    double max_back = 0; //maior termo do maxback_val, acho que fazer assim fica mais clean
+    
     for(int i = 0; i < n; i++){
-        if(find(solution_init.begin(), solution_init.end(), i) != solution_init.end()){
+        if(connected[i]){
             for(int j = i + 1; j < n; j++){
-                maxback_val[i] += x[i][j];
+                maxback_val[j] += x[i][j]; 
             }
         }
         cout << maxback_val[i] << " ";
     }
 
     double cut_val = 0; // variável que diz o somatório de todas as conexões dos que não estão conectados com a solução
-    //for para definir cut val
     for(int i = 0; i < n; i++){
         for(int j = i+1; j < n; j++){
-            if(find(solution_init.begin(), solution_init.end(), i) != solution_init.end()){ //roubado dnv! mt pesado
-                if(find(solution_init.begin(), solution_init.end(), j) == solution_init.end())
+            if(connected[i] && !connected[j]){ 
                 cut_val += x[i][j];
             }
         }
     }
 
-    cout << "\nCut val: " << cut_val;
+    cout << "\nCut val: " << cut_val << endl;
 
     double mincut_val = cut_val;
 
-    vector<double> solution = solution_init;
+    auto solution = solution_init;
 
     auto local_solution = solution_init;
 
     for(int i = 0; i < (n - solution_init.size()); i++){
         auto max_index = max_element(maxback_val.begin(), maxback_val.end());
+        cout << "Max: " << std::distance(maxback_val.begin(), max_index) << "\n";
 
-        local_solution.insert(local_solution.end(), *max_index); //não é o valor máximo, é o indice do valor máximo
+        auto max_value = std::distance(maxback_val.begin(), max_index);
+
+        for (auto a : maxback_val){
+            cout << a << ' ';
+        }cout << endl;
+
+        local_solution.push_back(std::distance(maxback_val.begin(), max_index)); //não é o valor máximo, é o indice do valor máximo
+        connected[max_value] = true;
+        cout << "Local: ";
+        for(auto i : local_solution){
+            cout << i << " -> "; 
+        }
+        cout << "\n";
+        unordered_set <double> local_set(local_solution.begin(), local_solution.end());
 
         cut_val = cut_val + 2 - 2 * (*max_index);
 
         for(int j = 0; j < n; j++){ //atualizar o max_back
-            if(find(solution_init.begin(), solution_init.end(), j) != solution_init.end()){
-
+            if(connected[j] == false){
+                if(j > max_value){
+                    maxback_val[j] += x[max_value][j];
+                } else{
+                    maxback_val[j] += x[j][max_value];
+                }
             }
         }
 
