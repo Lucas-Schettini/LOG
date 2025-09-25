e12d84d6fe619654cfbd0fa8705d7420fed72a96
Lucas Oliveira <lucas@q9.logq.int>
2025-09-25 15:54:27 -0300

    Testes para retornar o corte melhor

--- Branch-and-Cut/src/separation.cpp
+++ Branch-and-Cut/src/separation.cpp
@@ -18,7 +18,7 @@ pair<vector<int>, double> OneTourMaxBack(int n, double ** x, int init, vector<bo
     for(int i = 0; i < n; i++){
         if(connected[i]){
             if(mincut && merge_sets[i].empty()){
-                cout << "Pulei1: " << i << " \n";
+                //cout << "Pulei1: " << i << " \n";
                 continue;
             } 
             for(int j = i + 1; j < n; j++){
@@ -30,6 +30,11 @@ pair<vector<int>, double> OneTourMaxBack(int n, double ** x, int init, vector<bo
 
     double cut_val = 0; // variável que diz o somatório de todas as conexões dos que não estão conectados com a solução
     for(int i = 0; i < n; i++){
+        if(mincut && merge_sets[i].empty()){
+                //cout << "Pulei1: " << i << " \n";
+                continue;
+        }
+
         for(int j = i+1; j < n; j++){
             if(connected[i] && !connected[j]){ 
                 cut_val += x[i][j];
@@ -43,7 +48,7 @@ pair<vector<int>, double> OneTourMaxBack(int n, double ** x, int init, vector<bo
     
     auto solution = local_solution;
 
-    for(int i = 0; i < (n); i++){
+    for(int i = 0; i < n; i++){
         auto max_index = max_element(maxback_val.begin(), maxback_val.end());
         //cout << "Max: " << std::distance(maxback_val.begin(), max_index) << "\n";
 
@@ -59,15 +64,16 @@ pair<vector<int>, double> OneTourMaxBack(int n, double ** x, int init, vector<bo
         maxback_val[max_value] = 0;
 
         for(int j = 0; j < n; j++){ //atualizar o max_back
-
+            
             if(mincut && merge_sets[j].empty()){
                 // if(j == 15){
                 //     cout << "Pulei2: " << j << " \n";
                 // }
+                maxback_val[j] = 0;
                 continue;
             } 
             if(connected[j] == false){
-                if(j > max_value){
+                if(j > max_value + EPSILON){
                     maxback_val[j] += x[max_value][j];
                 } else{
                     maxback_val[j] += x[j][max_value];
@@ -75,22 +81,26 @@ pair<vector<int>, double> OneTourMaxBack(int n, double ** x, int init, vector<bo
             }
         }
 
+        // for(auto a : maxback_val){
+        //     cout << a << " ";
+        // } cout << "\n";
+
         if(cut_val < mincut_val + EPSILON){
             mincut_val = cut_val;
             solution = local_solution;
 
-            //cout << "solution: ";
+            // cout << "solution: ";
 
             // for(auto i : solution){
             //     cout << i << " -> "; 
             // }
 
-            //cout << "\n";
+            // cout << "\n";
         }
 
         if(cut_val < EPSILON && !mincut){
             //subtours.push_back(solution);
-            for(int i = 0; i < solution.size(); i++){ // ta errado
+            for(int i = 0; i < solution.size(); i++){
                 visited[solution[i]] = 1;
             }
             // cout << "Visitados: ";
@@ -100,9 +110,9 @@ pair<vector<int>, double> OneTourMaxBack(int n, double ** x, int init, vector<bo
             return make_pair(solution, cut_val);
         }
     }
-    for(auto a : solution){
-        cout << a << " -> ";
-    } cout << '\n';
+    // for(auto a : solution){
+    //     cout << a << " -> ";
+    // } cout << '\n';
 
     return make_pair(solution, cut_val);
 }
@@ -173,9 +183,9 @@ vector <vector<int> > MaxBack(double** x, int n){
 void Shrink(double ** x_mincut, int n, const vector<int>& max_back_tour, vector<vector<int>>& merge_sets){
 
     int s = max_back_tour.back();
-    cout << "s: " << s;
+    //cout << "s: " << s;
     int t = max_back_tour[max_back_tour.size()-2];
-    cout << " t: " << t << endl;
+    //cout << " t: " << t << endl;
 
     int temp = t;
     
@@ -255,22 +265,22 @@ vector <vector<int> > MinCut(double** x, int n){
     }
 
     for(int i = 0; i < n; i++){
-        cout << "Conjuntos de nós fundidos: " << endl;
+        //cout << "Conjuntos de nós fundidos: " << endl;
 
-        for(int i = 0; i < merge_sets.size(); i++){
-            if(merge_sets[i].empty()){
-                continue;
-            }
-            for(int j = 0; j < merge_sets[i].size(); j++){
-                cout << merge_sets[i][j] << " ";
-            }
-            cout << "\n";
-        }
+        // for(int i = 0; i < merge_sets.size(); i++){
+        //     if(merge_sets[i].empty()){
+        //         continue;
+        //     }
+        //     for(int j = 0; j < merge_sets[i].size(); j++){
+        //         cout << merge_sets[i][j] << " ";
+        //     }
+        //     cout << "\n";
+        // }
 
         auto local_solution = OneTourMaxBack(n, x_mincut , 0, visited, 1, merge_sets);
 
         auto max_back_tour = local_solution.first;
-        cout << "Tour(" << max_back_tour.size() << "): \n";
+        //cout << "Tour(" << max_back_tour.size() << "): \n";
         // for(auto a : max_back_tour){
         //     cout << a << " -> ";
         // }cout << "\n";
@@ -285,19 +295,43 @@ vector <vector<int> > MinCut(double** x, int n){
 
         if(cut_val < mincut_val){
             mincut_val = cut_val;
-            subtours.push_back(max_back_tour);
+            //subtours.push_back(max_back_tour);
+            subtours = merge_sets;
+
+            // for(int k = 0; k < merge_sets.size(); k++){
+            //     subtours.push_back(merge_sets[i]);
+            // }
+            
         }
 
-        Shrink(x_mincut, n, max_back_tour, merge_sets);
+        if(merge_sets[0].size() != n){
+            Shrink(x_mincut, n, max_back_tour, merge_sets);
+        }        
 
     }
 
-    // if(subtours.back().size() == n){
-    //     return {};
+    vector<vector<int>> subtours_clean;
+
+    for(int i = 0; i < subtours.size(); i ++){
+        if(!subtours[i].empty()){
+            subtours_clean.push_back(subtours[i]);
+        }
+    }
+
+    // for(int i = 0; i < subtours_clean.size(); i++){
+    //     for(int j = 0; j < subtours_clean[i].size(); j++){
+    //         cout << subtours_clean[i][j] << " ";
+    //     }
+    //     cout << "\n";
     // }
 
+    if(subtours_clean.back().size() == n){
+        cout << "RETORNEI\n\n\n\n";
+        return{};
+    }
 
-    return merge_sets;
+    //return merge_sets;
+    return subtours_clean;
 }
 
 
