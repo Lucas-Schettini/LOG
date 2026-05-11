2391b23b5edf87734a3d5ab0d560211b46a12ee3
Lucas Oliveira <lucas@q9.logq.int>
2026-05-11 13:58:32 -0300

    Fase 1 inicio

--- Revised_Simplex/src/aux_functions.h
+++ Revised_Simplex/src/aux_functions.h
@@ -1,6 +1,11 @@
 #include "fact.h"
 #include <cmath>
 #include <iomanip>
+#include <algorithm>
+
+#define PINF 99999999999
+#define NINF -99999999999
+
 using Eigen::RowVectorXd;
 
 struct EtaFactor{
@@ -10,3 +15,4 @@ struct EtaFactor{
 
 void solve_y(RowVectorXd& y, vector<EtaFactor>& eta_list, FactControl& fact, MatrixXd& A);
 void solve_d(VectorXd& d, VectorXd& a, vector<EtaFactor>& eta_list, FactControl& fact, MatrixXd& A);
+pair<double, VectorXd> revised_simplex(MatrixXd& A, VectorXd& b, VectorXd& c, VectorXd& lb, VectorXd& ub);
